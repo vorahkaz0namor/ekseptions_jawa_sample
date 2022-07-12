91254cc39e726a86a340be0bf8a7d3c0432d4c2d
Захаров Роман AN-34 <ambeadexter@yandex.ru>
2022-07-12 21:45:33 +0700

    Exotic solution

--- src/KataFour.java
+++ src/KataFour.java
@@ -2,7 +2,9 @@ public class KataFour {
     public static void main(String[] args) {
         int[] numz = {8, 3, -5, 42, -1, 0, 0, -9, 4, 7, 4, -4};
         int smallest = 0;
-
+        smallest = Integer.MAX_VALUE;
+        for (int i : numz)
+            smallest = Math.min(smallest, i);
         System.out.println(smallest);
     }
 }
