bca37bb5bd40e3491feb01c4f7d1c1980e4463cd
Захаров Роман AN-34 <ambeadexter@yandex.ru>
2022-07-12 21:29:37 +0700

    Ready to branching?

---
+++ src/KataFour.java
@@ -0,0 +1,8 @@
+public class KataFour {
+    public static void main(String[] args) {
+        int[] numz = {8, 3, -5, 42, -1, 0, 0, -9, 4, 7, 4, -4};
+        int smallest = 0;
+
+        System.out.println(smallest);
+    }
+}
