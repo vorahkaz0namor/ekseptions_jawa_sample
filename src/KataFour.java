0658a427fb387f6e966d6557534c41b878954f22
Захаров Роман AN-34 <ambeadexter@yandex.ru>
2022-07-12 21:43:06 +0700

    Shortest solution

--- src/KataFour.java
+++ src/KataFour.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
+
 public class KataFour {
     public static void main(String[] args) {
         int[] numz = {8, 3, -5, 42, -1, 0, 0, -9, 4, 7, 4, -4};
         int smallest = 0;
-
+        smallest = (Arrays.stream(numz).min().getAsInt());
         System.out.println(smallest);
     }
 }
