3683d122e0de7e082d28003d6f4f8fac0c26db79
Захаров Роман AN-34 <ambeadexter@yandex.ru>
2022-07-17 20:40:53 +0700

    Added Iterator tests method

--- src/KataFour.java
+++ src/KataFour.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
 
 public class KataFour {
     public static void main(String[] args) {
         System.out.println("Checking pre- and post- increments:");
         checkPrePost();
+        System.out.println("Iterator tests:");
+        iteratorTests();
     }
 
     public static void checkPrePost() {
@@ -12,5 +17,31 @@ public class KataFour {
         int y2 = x2++;
         int[] xy = {x1, y1, x2, y2};
         for (int n : xy) System.out.println(n);
+    }
+
+    public static void iteratorTests() {
+        List<String> spisok = new ArrayList<>();
+        spisok.add("qwe");
+        spisok.add("asd");
+        spisok.add("zxc");
+        spisok.add("rty");
+        spisok.add("fgh");
+        spisok.add("vbn");
+        spisok.add("uio");
+        spisok.add("jkl");
+        spisok.add("mp");
+        int i = 35;
+        ListIterator<String> lite = spisok.listIterator();
+        lite.forEachRemaining(System.out::println);
+        lite = spisok.listIterator();
+        while (lite.hasNext()) {
+            i = i + 3;
+            char s = (char) i;
+            lite.next();
+            lite.set(String.valueOf(s));
+        }
+        while (lite.hasPrevious())
+            lite.previous();
+        lite.forEachRemaining(System.out::print);
     }
 }
