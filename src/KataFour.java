50f93c7b0e48a094728886ec5ed153ba05ac2e3d
Захаров Роман AN-34 <ambeadexter@yandex.ru>
2022-07-17 20:10:12 +0700

    Removed sorting

--- src/KataFour.java
+++ src/KataFour.java
@@ -2,14 +2,7 @@ import java.util.Arrays;
 
 public class KataFour {
     public static void main(String[] args) {
-        int[] numz = {8, 3, -5, 42, -1, 0, 0, -9, 4, 7, 4, -4};
-        int smallest = 0;
-        smallest = (Arrays.stream(numz).min().getAsInt());
-        System.out.println(smallest);
-        smallest = Integer.MAX_VALUE;
-        for (int i : numz)
-            smallest = Math.min(smallest, i);
-        System.out.println(smallest);
+        System.out.println("Checking pre- and post- increments:");
         checkPrePost();
     }
 
